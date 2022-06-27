7d94564667559f95fda2a887519a09787b2bd5fb
Changzhanhe <changxiaohe1122@icloud.com>
2022-06-27 18:03:43 +0800

    Fix bugs

--- MultiSpace/start.py
+++ MultiSpace/start.py
@@ -8,9 +8,8 @@ import argparse as ap
 
 from MultiSpace.version import __version__
 from MultiSpace.MultiSpace_init.MultiSpace_init import pipelineinit_parser,PipelineConfig
-from MultiSpace.GeneMethyscore.GeneMethyscore import methratio_parser,WCGMethyMatrix
-from MultiSpace.GeneActivity.GeneActivity import geneactivity_parser, GCHScoreMatrix
-from MultiSpace.MappingscCell.MappingscCell import gettingepisignal_parser, Mapping
+from MultiSpace.Generatescorematrix.Generatescorematrix import generatescore_parser,Generate_scorematrix
+from MultiSpace.MappingscCell.MappingscCell import mappingcelltospatial_parser, Mapping
 
 
 def main():
@@ -24,9 +23,8 @@ def main():
     subparsers = parser.add_subparsers(dest = "subcommand")
 
     pipelineinit_parser(subparsers)
-    methratio_parser(subparsers)
-    geneactivity_parser(subparsers)
-    gettingepisignal_parser(subparsers)
+    generatescore_parser(subparsers)
+    mappingcelltospatial_parser(subparsers)
 
     logging.basicConfig(format="%(levelname)s: %(message)s", stream=sys.stderr)
     args = parser.parse_args()
@@ -36,16 +34,13 @@ def main():
     if args.version:
         print(version)
         exit(0)
-    elif args.subcommand == "pipeline_init":
+    elif args.subcommand == "Pipelineinit":
         PipelineConfig(species = args.species, samplesheet = args.samplesheet, star_annotation = args.star_annotation, 
             fasta = args.fasta, lambda_fasta = args.lambda_fasta, fasta_fai = args.fasta_fai, star_index = args.star_index, directory = args.directory)
-    elif args.subcommand == "gch_geneactivity":
-        GCHScoreMatrix(file_path = args.file_path , species = args.species, cell_barcode = args.cell_barcode, out_dir = args.out_dir, 
-            out_prefix = args.out_prefix, distance = args.distance)
-    elif args.subcommand == "wcg_methratio":
-        WCGMethyMatrix(peak_reference = args.peak_reference, species = args.species, meth_matrix = args.meth_matrix, cell_barcode = args.cell_barcode, 
-            out_dir = args.out_dir, out_prefix = args.out_prefix, region = args.region, distance = args.distance)
-    elif args.subcommand == "getting_episignal":
+    elif args.subcommand == "Scorematrix":
+        Generate_scorematrix(file_path = args.file_path , species = args.species, cell_barcode = args.cell_barcode, out_dir = args.out_dir, matrixtype = args.matrixtype,
+            out_prefix = args.out_prefix, distance = args.distance, region = args.region)
+    elif args.subcommand == "Mappingcell":
         Mapping(sc_count_file = args.sc_count_file , sc_anno_file = args.sc_anno_file, st_count_file = args.st_count_file, sc_scale_factor = args.sc_scale_factor, 
             st_scale_factor = args.st_scale_factor, out_dir = args.out_dir, out_prefix = args.out_prefix, 
             model_dir = args.model_dir, normalize = args.normalize, gene_use = args.gene_use, ntopics_list = args.ntopics_list, spatial_location = args.spatial_location,
