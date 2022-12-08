d8d8dcdbbb0caed449dd07c337ad81d881140a7e
Changzhanhe <changxiaohe1122@icloud.com>
2022-12-08 10:56:59 +0800

    modified

--- MultiSpace/MultiSpace_init/MultiSpace_init.py
+++ MultiSpace/MultiSpace_init/MultiSpace_init.py
@@ -98,9 +98,9 @@ def PipelineConfig(species, samplesheet, rna_annotation, fasta, fasta_fai, lambd
 				rna_annotation = os.path.abspath(rna_annotation),
 				star_index = os.path.abspath(star_index),
 				repeat_line = os.path.abspath(repeatLINEfile),
-				repeat_ltr = os.path.abspath(repeatLTRfile),
-				dnadir = os.path.abspath(DNAdir),
-				rnadir = os.path.abspath(RNAdir)))
+				repeat_ltr = os.path.abspath(repeatLTRfile)))
+				# dnadir = os.path.abspath(DNAdir),
+				# rnadir = os.path.abspath(RNAdir)))
 
 
 	source = os.path.join(pkgpath, "Snakefile")
